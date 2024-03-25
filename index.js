cda36bb066e2e41cd44407cd7095b0954b0dd4d0
Alvin Angulu <angulualvin@gmail.com>
2024-03-25 22:06:54 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <=400){
+    const msg = 'This one is on me!';
+    return msg;
+  }
+  else if(ride>=400 && ride <=2000){
+    const msg = 'That will be twenty bucks.'
+    return msg;
+  }
+  else if(ride>2000 && ride <2500){
+    const msg = 'I will gladly take your thirty bucks.';
+    return msg;
+  }
+  else {
+    const msg = 'No can do.';
+    return msg;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+   return (city ==="NYC") ? "Ok, sounds good." : 'No go.' ;
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous" : 
+    return "Thank you so much.";
+    case "not as generous" :
+      return "Thank you.";
+    default :
+    return "Bye.";
+  }
 }
+
+console.log(switchOnCharmFromTip("not as generous"));
